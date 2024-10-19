05e01b36c68b47bcd4aa8308096480dfbafe2652
Piyush Bagde <piyushbagde002@gmail.com>
2024-10-19 17:58:34 +0530

    added image previewing functionality

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from ultralytics import YOLO
 import os
 
 app = Flask(__name__)
-MODEL_PATH = 'model/best.pt'  # Path to your .pt model
-model = YOLO(MODEL_PATH)  # Load the model once, globally
+MODEL_PATH = 'model/model.pt'
+model = YOLO(MODEL_PATH)
 
 UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create uploads directory if it doesn't exist
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         if 'file' not in request.files:
             return render_template('index.html', error="No file part")
+
         file = request.files['file']
         if file.filename == '':
             return render_template('index.html', error="No selected file")
-        if file:  # Check if the file is actually uploaded
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
+        if file:
+            filename = file.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
 
-            # Run inference
-            results = model(filepath)  # Results object with predictions
+            results = model(filepath, conf=0.7)
 
-            # Save annotated image
-            annotated_image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'annotated_' + file.filename)
-            results[0].save(annotated_image_path)  # Save the first image (index 0)
+            annotated_filename = 'annotated_' + filename
+            annotated_image_path = os.path.join(app.config['UPLOAD_FOLDER'], annotated_filename)
+            results[0].save(annotated_image_path)
 
-            return render_template('index.html', image_path='uploads/annotated_' + file.filename)
+            return redirect(url_for('show_results',
+                                    original_image_name=filename,
+                                    annotated_image_name=annotated_filename))
 
     return render_template('index.html')
 
 
-# Serving uploaded/processed files
+@app.route('/results/<original_image_name>/<annotated_image_name>')
+def show_results(original_image_name, annotated_image_name):
+    return render_template('results.html',
+                           original_image_name=original_image_name,
+                           annotated_image_name=annotated_image_name)
+
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
